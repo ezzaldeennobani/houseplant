1b8a29fe2739f67d344da9880609feb49264c447
ThisIsLion <110822592+ThisIsLion@users.noreply.github.com>
2024-09-07 18:14:26 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import ProductListingPage from './pages/ProductListingPage';
-import ShoppingCartPage from './pages/ShoppingCartPage';
-import Header from './components/Header';
+import LandingPage from './pages/LandingPage'; // Import your landing page component
+import ProductListingPage from './pages/ProductListingPage'; // Import your product listing page
+import ShoppingCartPage from './pages/ShoppingCartPage'; // Import your shopping cart page
+import Header from './components/Header'; // Import your header component
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header /> {/* Display the header on every page */}
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductListingPage />} />
-        <Route path="/cart" element={<ShoppingCartPage />} />
+        <Route path="/" element={<LandingPage />} /> {/* Display landing page at the root */}
+        <Route path="/products" element={<ProductListingPage />} /> {/* Display product listing */}
+        <Route path="/cart" element={<ShoppingCartPage />} /> {/* Display shopping cart */}
       </Routes>
     </Router>
   );
